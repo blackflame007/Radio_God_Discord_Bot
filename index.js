5b2a993ee44b89a1a4dc3c7624a48b18797f4dcf
Nick L <lesseosnick@gmail.com>
2018-12-15 02:30:52 -0800

    Add Join channel function and leave

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 import axios from 'axios';
 import Discord from 'discord.js';
+const opusscript = require('opusscript');
 import { TOKEN, dirble_key, prefix } from './constants.js';
+
 const client = new Discord.Client();
 
+global.servers = {};
+
+const samplingRate = 48000;
+const frameDuration = 20;
+const channels = 2;
+
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(
+    `Logged in as ${client.user.tag} (${client.user.id} on ${
+      client.guilds.size
+    })`
+  );
+  client.user.setActivity(
+    `${prefix}help | Active Servers ${client.guilds.size}`
+  );
 });
 
 client.on('message', msg => {
@@ -12,17 +27,57 @@ client.on('message', msg => {
 
   const args = msg.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
-  if (command === 'search') {
-    axios
-      .post(`http://api.dirble.com/v2/search?token=${dirble_key}`, {
-        query: 'KLOS'
-      })
-      .then(function(response) {
-        console.log(response);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+
+  if (!msg.guild.voiceConnection) {
+    if (!servers[msg.guild.id]) {
+      servers[msg.guild.id] = { queue: [] };
+    }
+    if (command === 'join') {
+      const server = servers[msg.guild.id];
+      msg.member.voice.channel
+        .join()
+        .then(() => {
+          msg.channel.send('Successfully Joined!');
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  } else {
+    msg.reply('You must be in a voice channel to summon me!');
+  }
+  if (msg.member.voiceChannel) {
+    if (!msg.guild.voiceConnection) {
+      if (command === 'search') {
+        axios
+          .post(`http://api.dirble.com/v2/search?token=${dirble_key}`, {
+            query: args[0]
+          })
+          .then(response => {
+            msg.channel.send(`Found ${response.data[0].name}`);
+            console.log(response.data[0]);
+          })
+          .catch(error => {
+            msg.channel.send('Query is missing');
+            console.log(error);
+          });
+      }
+    }
+  }
+
+  if (msg.guild.voiceConnection) {
+    if (command === 'leave') {
+      msg.guild.voiceConnection.disconnect();
+    } else {
+      msg.reply('I must be in a voice channel to leave');
+    }
+  }
+
+  if (command === 'ping') {
+    const then = Date.now();
+    msg.channel.send('pinging ... ').then(m => {
+      m.edit(`Pong! it took ${Date.now() - then}ms to send that message!`);
+    });
   }
 });
 
